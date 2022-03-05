6b6e02af7008191dd021267f97ace1badab01bca
mkane40 <40065865+mkane40@users.noreply.github.com>
2022-03-05 15:14:03 -0600

    working update

--- js/main.js
+++ js/main.js
@@ -7,12 +7,16 @@ function createMap() {
         center: [20, 0],
         zoom: 2
     });
-
+    
     //add OSM base tilelayer
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
     }).addTo(map);
-
+    L.Control.geocoder().addTo(map);
+    
+    
+    
+    //L.Control.layers(overlayMaps, {position: 'topleft'})addTo(map);
     //call getData function
     getData(map);
 }
@@ -29,16 +33,14 @@ function calcPropRadius(attValue) {
    return radius;
 }
 
-    //check result
-    console.log("here2");
 
 function createPopup(properties, attribute, layer, radius){
     //add city to popup content string
-    var popupContent = "<p><b>Country:</b> " + properties.Country + "</p>";
+    var popupContent = "<p><b>County: </b>" + properties.Country +"</p> ";
 
     //add formatted attribute to panel content string
     var year = attribute.split("_")[1];
-    popupContent += "<p><b>Fertility Rate in " + year + ":</b> " + properties[attribute] + " Per Family</p>";
+    popupContent += "<p><b>Year " + year + ":</b> " + properties[attribute] + " Per Women</p>";
 
     //bind the popup to the circle marker
     layer.bindPopup(popupContent, {
@@ -63,18 +65,17 @@ function pointToLayer(feature, latlng, attributes) {
     
     //create marker options
     var attribute = attributes[0];
-    console.log(attribute);
-    
     
     //marker options style
     var geojsonMarkerOptions = {
         radius: 8,
-        fillColor: "#C9F2D7",
+        fillColor: "#81A6A6",
         color: "#000",
         weight: 1,
         opacity: 0.8,
         fillOpacity: 0.8
     };
+    
     //For each feature, determine its value for the selected attribute
     var attValue = Number(feature.properties[attribute]);
         
@@ -143,9 +144,10 @@ function updatePropSymbols(map, attribute){
 
             //Example 1.3 line 6...in UpdatePropSymbols()
             createPopup(props, attribute, layer, radius);
+            
+            updateLegend(map, attribute);
         };
     });
-    updateLegend(map, attribute);
 };
 
 function createLegend(map, attributes){
@@ -160,23 +162,103 @@ function createLegend(map, attributes){
 
             //PUT YOUR SCRIPT TO CREATE THE TEMPORAL LEGEND HERE
             $(container).append('<div id="temporal-legend">')
+            
+            //Step 1: start attribute legend svg string
+            var svg = '<svg id="attribute-legend" width="160px" height="70px">';
+            
+            //object to base loop on...replaces Example 3.10 line 1
+            var circles = {
+                max: 20,
+                mean: 40,
+                min: 60
+            };
+    
+            //loop to add each circle and text to svg string
+            for (var circle in circles){
+                //circle string
+                svg += '<circle class="legend-circle" id="' + circle + '" fill="#81A6A6" fill-opacity="0.8" stroke="#000000" cx="30"/>';
+    
+                //text string
+                svg += '<text id="' + circle + '-text" x="65" y="' + circles[circle] + '"></text>';
+            };
+
+            //close svg string
+            svg += "</svg>";
+
+            //add attribute legend svg to container
+            $(container).append(svg);
 
             return container;
         }
     });
 
     map.addControl(new LegendControl());
+    
     updateLegend(map, attributes[0]);
 };
+
+
+//Calculate the max, mean, and min values for a given attribute
+function getCircleValues(map, attribute){
+    //start with min at highest possible and max at lowest possible number
+    var min = Infinity,
+        max = -Infinity;
+
+    map.eachLayer(function(layer){
+        //get the attribute value
+        if (layer.feature){
+            var attributeValue = Number(layer.feature.properties[attribute]);
+
+            //test for min
+            if (attributeValue < min){
+                min = attributeValue;
+            };
+
+            //test for max
+            if (attributeValue > max){
+                max = attributeValue;
+            };
+        };
+    });
+
+    //set mean
+    var mean = (max + min) / 2;
+
+    //return values as an object
+    return {
+        max: max,
+        mean: mean,
+        min: min
+    };
+};
+
 
 function updateLegend(map, attribute){
     //create content for legend
     var year = attribute.split("_")[1];
-    var content = "Population in " + year;
+    var content = "<p><b>Year: </b>" + year;
+    
     $('#temporal-legend').html(content);
+    
+    //get the max, mean, and min values as an object
+    var circleValues = getCircleValues(map, attribute);
+    
+    for (var key in circleValues){
+        //get the radius
+        var radius = calcPropRadius(circleValues[key]);
+
+        //Step 3: assign the cy and r attributes
+        $('#'+key).attr({
+            cy: 50 - radius,
+            r: radius
+        });
+        
+         //Step 4: add legend text
+        $('#'+key+'-text').text(Math.round(circleValues[key]*100)/100 + " Per Women");
+    };
 };
 
-function createSequenceControls(map){
+function createSequenceControls(map, attributes){
      var SequenceControl = L.Control.extend({
         options: {
             position: 'bottomleft'
@@ -197,6 +279,16 @@ function createSequenceControls(map){
             $(container).on('mousedown dblclick', function(e){
                 L.DomEvent.stopPropagation(e);
             });
+            
+            $(container).on('mouseover', function () {
+                map.dragging.disable();
+
+            });
+            
+             $(container).on('mouseout', function () {
+                map.dragging.enable();
+            });
+            
             return container;
         }
     });
@@ -207,6 +299,14 @@ function createSequenceControls(map){
     //$('#panel').append('<input class="range-slider" type="range">');
     //$('#panel').append('<button class="skip" id="reverse">❮</button>');
     //$('#panel').append('<button class="skip" id="forward">❯</button>');
+    
+    //set up slider properties
+    $('.range-slider').attr({
+        max: 7,
+        min: 0,
+        value: 0,
+        step: 1
+    });
     
     $('.skip').click(function(){
         //sequence
@@ -221,7 +321,12 @@ function createSequenceControls(map){
             //Step 7: if past the first attribute, wrap around to last attribute
             index = index < 0 ? 6 : index;
         };
+        
         $('.range-slider').val(index);
+        
+        console.log(attributes);
+        console.log(index);
+        console.log(attributes[index])
         updatePropSymbols(map, attributes[index]);
     });
     
@@ -232,13 +337,6 @@ function createSequenceControls(map){
         updatePropSymbols(map, attributes[index]);
     });
     
-    //set up slider properties
-    $('.range-slider').attr({
-        max: 7,
-        min: 0,
-        value: 0,
-        step: 1
-    });
 };
 
 
@@ -250,9 +348,11 @@ function getData(map){
         success: function(response){
             //add array of attributes
             var attributes = processData(response);
+            
             //call function to create proportional symbols
             createPropSymbols(response, map, attributes);
             createSequenceControls(map, attributes);
+            createLegend(map,attributes);
         }
     });
 };
